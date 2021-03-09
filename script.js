00dca412874867a8b1163e553309aadec08ac901
Aykut Terzi <terziaykut@hotmail.com>
2021-03-09 13:25:35 +0300

    Refactor smooth scrolling for nav links

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,9 +32,7 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
+// Button Scrolling
 btnScrollTo.addEventListener("click", function (e) {
   // const s1coords = section1.getBoundingClientRect();
   // console.log(s1coords);
@@ -62,4 +62,31 @@ btnScrollTo.addEventListener("click", function (e) {
 
   // This is the new way. Only supported by modern browsers
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//////////////////////////////
+// Page navigation
+
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    e.preventDefault();
+    const id = e.target.getAttribute("href");
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
